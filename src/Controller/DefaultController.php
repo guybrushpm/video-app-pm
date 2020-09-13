0ac27993bfaa95c5ddf50004d66f9acf0c35a4cb
guybrushpm <phil.moritz@gmx.de>
2020-09-13 19:37:43 +0200

    changed swapped videos :)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -9,8 +9,8 @@ class DefaultController extends AbstractController
     public function index()
     {
         $videos = [
-              'video1' => ['name' => 'Sweeping Arches and Loops', 'file' => 'video1.mp4'],
-              'video2' => ['name' => 'Black Holes Dancing with Stars', 'file' => 'video2.mp4']
+              'video1' => ['name' => 'Black Holes Dancing with Stars', 'file' => 'video1.mp4'],
+              'video2' => ['name' => 'Sweeping Arches and Loops', 'file' => 'video2.mp4']
               ];
 
         return $this->render('homepage.html.twig', ['videos' => $videos]);
--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -14,8 +14,8 @@ class VideoController extends AbstractController
             'player' => '/static/plyr/'
             ];
         $videos = [
-            'video1' => ['name' => 'Sweeping Arches and Loops', 'file' => 'video1.mp4'],
-            'video2' => ['name' => 'Black Holes Dancing with Stars', 'file' => 'video2.mp4']
+            'video1' => ['name' => 'Black Holes Dancing with Stars', 'file' => 'video1.mp4'],
+            'video2' => ['name' => 'Sweeping Arches and Loops', 'file' => 'video2.mp4']
          ];
          $videodata = $videos[$video];
 

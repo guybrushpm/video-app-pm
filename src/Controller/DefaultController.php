5adcc315116bf5f2bf7ad699129f4a2222d53d98
guybrushpm <phil.moritz@gmx.de>
2020-09-13 18:48:54 +0200

    cleanup

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,7 +8,6 @@ class DefaultController extends AbstractController
 {
     public function index()
     {
-        
         $videos = [
               'video1' => ['name' => 'Sweeping Arches and Loops', 'file' => 'video1.mp4'],
               'video2' => ['name' => 'Black Holes Dancing with Stars', 'file' => 'video2.mp4']
--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -17,7 +17,6 @@ class VideoController extends AbstractController
             'video1' => ['name' => 'Sweeping Arches and Loops', 'file' => 'video1.mp4'],
             'video2' => ['name' => 'Black Holes Dancing with Stars', 'file' => 'video2.mp4']
          ];
-
          $videodata = $videos[$video];
 
         return $this->render('video.html.twig', ['videodata' => $videodata,  'paths' => $paths]);

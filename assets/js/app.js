db5c981b573a7ce5d1d8a1687e5747048740f796
guybrushpm <phil.moritz@gmx.de>
2020-09-13 19:32:33 +0200

    switch css to scss

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,8 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/bootstrap.min.css';
-import '../css/app.css';
+import '../css/bootstrap.min.scss';
+import '../css/app.scss';
 
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
